2304faefefe4439c07e80aacef3e3bfea852031b
ElZorro <thiago2007crackz@gmail.com>
2025-09-16 17:09:45 -0300

    Update Create-Producto.js

--- Frontend/WEB/admin/CRUDProducto/Create-Producto.js
+++ Frontend/WEB/admin/CRUDProducto/Create-Producto.js
@@ -179,10 +179,6 @@ if (form) {
       const descripcion = document.getElementById("descripcion").value.trim();
       const categoriaId = parseInt(document.getElementById("categoria").value);
       const precio = parseFloat(document.getElementById("precio").value);
-      const precioDigitalInput = document.getElementById("precioDigital");
-      if (precioDigitalInput) {
-        precioDigitalInput.value = producto.precioDigital || "";
-      }
       const codigoInicial = document.getElementById("codigo-inicial").value.trim();
       const version = document.getElementById("version").value.trim();
       const seguimiento = document.getElementById("seguimiento").value.trim();
